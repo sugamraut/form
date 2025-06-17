a1f11e3a5b650f26243cddee6faad909acbbac2d
Sugam Raut <sugamrautbim@gmail.com>
2025-06-17 16:33:53 +0545

    feat: adding  the sass on login.scss and the fetch the data from the commponent of sccs ie _border,_font,_display

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import '../src/assets/css/main.css'
+import '../src/assets/css/main.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/log-in/login.tsx
+++ src/pages/log-in/login.tsx
@@ -1,4 +1,4 @@
-import "../../assets/css/login.css";
+import "../../assets/css/login.scss";
 import Form from "../../components/form";
 
 function Login() {
