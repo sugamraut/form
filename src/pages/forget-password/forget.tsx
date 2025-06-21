41256c1dd5907d4710409da435ea969b604b0dae
Sugam Raut <sugamrautbim@gmail.com>
2025-06-21 16:35:51 +0545

    feat: adding the bootstrap css on the forget.tsx

--- src/pages/forget-password/forget.tsx
+++ src/pages/forget-password/forget.tsx
@@ -1,19 +1,18 @@
 import top from "../../assets/images/top.png";
 import "../../assets/css/forget.scss";
-import sidevideos from "../../assets/images/sidevideo.mp4";
+import Sidevideo from "../../components/Sidevideo";
+
 function Forget() {
   return (
     <>
-      <div className="container">
-        <div className="box-section">
-          <div className="video-section">
-            <video autoPlay muted loop playsInline>
-              <source src={sidevideos} type="video/mp4" />
-            </video>
+      <div className="container-fluid">
+        <div className="box-section  d-xxl-flex d-md-flex d-sm-flex overflow-hidden">
+          <div className="video-section flex-grow-1 flex-shrink-1">
+            <Sidevideo/>
           </div>
           {/*right side image */}
-          <div className="form-section">
-            <form className="form-container">
+          <div className="form-section row align-items-center justify-content-center flex-grow-1 flex-shrink-1">
+            <form className="form-container w-100">
               <div className="logo-container">
                 <img
                   src={top}
@@ -38,7 +37,7 @@ function Forget() {
                 <input type="radio" name="recovery" value="phone" />
                 Phone number
               </label>
-              <button className="button">Next</button>
+              <button className="button w-100">Next</button>
               <a href="/" className="link">
                 ← Back to login{" "}
               </a>
